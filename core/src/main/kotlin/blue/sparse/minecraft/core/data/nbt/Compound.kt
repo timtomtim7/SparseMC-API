96446c4bdede0e94118a87bdc5e575ee6bcdc3ed
Tom <tomhazredstone@hotmail.com>
2018-07-24 19:48:56 -0500

    Added default and editCompound methods to `Compound`

--- core/src/main/kotlin/blue/sparse/minecraft/core/data/nbt/Compound.kt
+++ core/src/main/kotlin/blue/sparse/minecraft/core/data/nbt/Compound.kt
@@ -1,11 +1,14 @@
 package blue.sparse.minecraft.core.data.nbt
 
-import blue.sparse.minecraft.core.data.DataMap
 import blue.sparse.minecraft.core.data.nbt.converter.NBTConverter
 import java.io.DataInputStream
 import java.io.File
 
-class Compound: DataMap<String> {
+@Suppress("UNCHECKED_CAST")
+class Compound/*: AbstractMutableMap<String, Any>*/ {
+
+//	override val entries: MutableSet<MutableMap.MutableEntry<String, Any>>
+//		get() = HashSet()
 
 	private val backingMap = LinkedHashMap<String, NBTValue<*>>()
 
@@ -27,7 +30,7 @@ class Compound: DataMap<String> {
 		}
 	}
 
-	override fun keys(): MutableSet<String> {
+	fun keys(): MutableSet<String> {
 		return backingMap.keys
 	}
 
@@ -35,197 +38,119 @@ class Compound: DataMap<String> {
 		return backingMap[key]!!
 	}
 
-	fun remove(key: String): Boolean {
+	/*override*/ fun remove(key: String): Boolean {
 		return backingMap.remove(key) != null
 	}
 
-	operator fun get(key: String): Any {
-		return backingMap[key]!!.value
+	/*override*/ fun put(key: String, value: Any): Any? {
+		val previous = getOptional(key)
+		convert(key, value)
+
+		return previous
 	}
 
-	fun getOptional(key: String): Any? {
-		return backingMap[key]?.value
+	/*override*/ operator fun get(key: String): Any {
+		return backingMap[key]!!.value
 	}
 
 	operator fun set(key: String, value: Any) {
 		backingMap[key] = NBTValue.toNBTValue(value)
 	}
 
-	override fun byte(key: String): Byte {
-		return (backingMap[key] as NBTValue.NBTByte).value
+	fun getOptional(key: String): Any? {
+		return backingMap[key]?.value
 	}
 
-	override fun short(key: String): Short {
-		return (backingMap[key] as NBTValue.NBTShort).value
+	fun default(key: String, value: Any): Any {
+		return backingMap.getOrPut(key) { NBTValue.toNBTValue(value) }.value
 	}
 
-	override fun int(key: String): Int {
-		return (backingMap[key] as NBTValue.NBTInt).value
+	inline fun default(key: String, valueSupplier: () -> Any): Any {
+		val result = getOptional(key)
+		if(result != null)
+			return result
+
+		val value = valueSupplier()
+		set(key, value)
+		return value
 	}
 
-	override fun long(key: String): Long {
-		return (backingMap[key] as NBTValue.NBTLong).value
-	}
+	fun byte(key: String): Byte = (backingMap[key] as NBTValue.NBTByte).value
+	fun short(key: String): Short = (backingMap[key] as NBTValue.NBTShort).value
+	fun int(key: String): Int = (backingMap[key] as NBTValue.NBTInt).value
+	fun long(key: String): Long = (backingMap[key] as NBTValue.NBTLong).value
+	fun float(key: String): Float = (backingMap[key] as NBTValue.NBTFloat).value
+	fun double(key: String): Double = (backingMap[key] as NBTValue.NBTDouble).value
+	fun string(key: String): String = (backingMap[key] as NBTValue.NBTString).value
+	fun byteArray(key: String): ByteArray = (backingMap[key] as NBTValue.NBTByteArray).value
+	fun shortArray(key: String): ShortArray = (backingMap[key] as NBTValue.NBTIntArray).value.map(Int::toShort).toShortArray()
+	fun intArray(key: String): IntArray = (backingMap[key] as NBTValue.NBTIntArray).value
+	fun longArray(key: String): LongArray = (backingMap[key] as NBTValue.NBTLongArray).value
+	fun compound(key: String): Compound = (backingMap[key] as NBTValue.NBTCompound).value
+	fun collection(key: String): Collection<Any> = (backingMap[key] as NBTValue.NBTList<*>).value
+	fun <T: Any> collectionTyped(key: String): Collection<T> = (backingMap[key] as NBTValue.NBTList<T>).value
+	fun convert(key: String): Any = optionalConvert(key)!!
 
-	override fun float(key: String): Float {
-		return (backingMap[key] as NBTValue.NBTFloat).value
-	}
+	inline fun <reified T: Any> convertTyped(key: String): T = optionalConvertTyped(key)!!
+	fun optionalByte(key: String): Byte? = getOptional(key) as? Byte
+	fun optionalShort(key: String): Short? = getOptional(key) as? Short
+	fun optionalInt(key: String): Int? = getOptional(key) as? Int
+	fun optionalLong(key: String): Long? = getOptional(key) as? Long
+	fun optionalFloat(key: String): Float? = getOptional(key) as? Float
+	fun optionalDouble(key: String): Double? = getOptional(key) as? Double
+	fun optionalString(key: String): String? = getOptional(key) as? String
+	fun optionalByteArray(key: String): ByteArray? = getOptional(key) as? ByteArray
+	fun optionalShortArray(key: String): ShortArray? = getOptional(key) as? ShortArray
+	fun optionalIntArray(key: String): IntArray? = getOptional(key) as? IntArray
+	fun optionalLongArray(key: String): LongArray? = getOptional(key) as? LongArray
+	fun optionalCompound(key: String): Compound? = getOptional(key) as? Compound
+	fun optionalCollection(key: String): Collection<Any>? = (backingMap[key] as? NBTValue.NBTList<*>)?.value
+	fun <T: Any> optionalCollectionTyped(key: String): Collection<T>? = (backingMap[key] as? NBTValue.NBTList<T>)?.value
+	fun optionalConvert(key: String): Any? = NBTConverter.getValue(this, key)
+	inline fun <reified T: Any> optionalConvertTyped(key: String): T? = optionalConvert(key) as? T
 
-	override fun double(key: String): Double {
-		return (backingMap[key] as NBTValue.NBTDouble).value
-	}
+	fun byte(key: String, value: Byte) { backingMap[key] = NBTValue.NBTByte(value) }
+	fun short(key: String, value: Short) { backingMap[key] = NBTValue.NBTShort(value) }
+	fun int(key: String, value: Int) { backingMap[key] = NBTValue.NBTInt(value) }
+	fun long(key: String, value: Long) { backingMap[key] = NBTValue.NBTLong(value) }
+	fun float(key: String, value: Float) { backingMap[key] = NBTValue.NBTFloat(value) }
+	fun double(key: String, value: Double) { backingMap[key] = NBTValue.NBTDouble(value) }
+	fun string(key: String, value: String) { backingMap[key] = NBTValue.NBTString(value) }
+	fun byteArray(key: String, value: ByteArray) { backingMap[key] = NBTValue.NBTByteArray(value) }
+	fun shortArray(key: String, value: ShortArray) { backingMap[key] = NBTValue.NBTIntArray(value.map(Short::toInt).toIntArray()) }
+	fun intArray(key: String, value: IntArray) { backingMap[key] = NBTValue.NBTIntArray(value) }
+	fun longArray(key: String, value: LongArray) { backingMap[key] = NBTValue.NBTLongArray(value) }
+	fun compound(key: String, value: Compound) { backingMap[key] = NBTValue.NBTCompound(value) }
+	inline fun compound(key: String, body: Compound.() -> Unit) { compound(key, Compound(body)) }
 
-	override fun string(key: String): String {
-		return (backingMap[key] as NBTValue.NBTString).value
-	}
+	fun defaultByte(key: String, value: Byte) = default(key, value) as Byte
+	fun defaultShort(key: String, value: Short) = default(key, value) as Short
+	fun defaultInt(key: String, value: Int) = default(key, value) as Int
+	fun defaultLong(key: String, value: Long) = default(key, value) as Long
+	fun defaultFloat(key: String, value: Float) = default(key, value) as Float
+	fun defaultDouble(key: String, value: Double) = default(key, value) as Double
+	fun defaultString(key: String, value: String) = default(key, value) as String
+	fun defaultByteArray(key: String, value: ByteArray) = default(key, value) as ByteArray
+	fun defaultShortArray(key: String, value: ShortArray) = default(key, value) as ShortArray
+	fun defaultIntArray(key: String, value: IntArray) = default(key, value) as IntArray
+	fun defaultLongArray(key: String, value: LongArray) = default(key, value) as LongArray
+	fun defaultCompound(key: String, value: Compound) = default(key, value) as Compound
+	inline fun defaultByte(key: String, supplier: () -> Long) = default(key, supplier) as Long
+	inline fun defaultShort(key: String, supplier: () -> Short) = default(key, supplier) as Short
+	inline fun defaultInt(key: String, supplier: () -> Int) = default(key, supplier) as Int
+	inline fun defaultLong(key: String, supplier: () -> Long) = default(key, supplier) as Long
+	inline fun defaultFloat(key: String, supplier: () -> Float) = default(key, supplier) as Float
+	inline fun defaultDouble(key: String, supplier: () -> Double) = default(key, supplier) as Double
+	inline fun defaultString(key: String, supplier: () -> String) = default(key, supplier) as String
+	inline fun defaultByteArray(key: String, supplier: () -> ByteArray) = default(key, supplier) as ByteArray
+	inline fun defaultShortArray(key: String, supplier: () -> ShortArray) = default(key, supplier) as ShortArray
+	inline fun defaultIntArray(key: String, supplier: () -> IntArray) = default(key, supplier) as IntArray
+	inline fun defaultLongArray(key: String, supplier: () -> LongArray) = default(key, supplier) as LongArray
+//	inline fun defaultCompound(key: String, supplier: () -> Compound) = default(key, supplier) as Compound
+	inline fun defaultCompound(key: String, body: Compound.() -> Unit) = default(key) { Compound(body) } as Compound
 
-	override fun byteArray(key: String): ByteArray {
-		return (backingMap[key] as NBTValue.NBTByteArray).value
-	}
-
-	override fun shortArray(key: String): ShortArray {
-		return (backingMap[key] as NBTValue.NBTIntArray).value.map(Int::toShort).toShortArray()
-	}
-
-	override fun intArray(key: String): IntArray {
-		return (backingMap[key] as NBTValue.NBTIntArray).value
-	}
-
-	override fun longArray(key: String): LongArray {
-		return (backingMap[key] as NBTValue.NBTLongArray).value
-	}
-
-	fun compound(key: String): Compound {
-		return (backingMap[key] as NBTValue.NBTCompound).value
-	}
-
-	fun collection(key: String): Collection<Any> {
-		return (backingMap[key] as NBTValue.NBTList<*>).value
-	}
-
-	fun convert(key: String): Any {
-		return optionalConvert(key)!!
-	}
-
-	inline fun <reified T: Any> convertTyped(key: String): T {
-		return optionalConvertTyped(key)!!
-	}
-
-
-	fun optionalByte(key: String): Byte? {
-		return getOptional(key) as? Byte
-	}
-
-	fun optionalShort(key: String): Short? {
-		return getOptional(key) as? Short
-	}
-
-	fun optionalInt(key: String): Int? {
-		return getOptional(key) as? Int
-	}
-
-	fun optionalLong(key: String): Long? {
-		return getOptional(key) as? Long
-	}
-
-
-	fun optionalFloat(key: String): Float? {
-		return getOptional(key) as? Float
-	}
-
-	fun optionalDouble(key: String): Double? {
-		return getOptional(key) as? Double
-	}
-
-
-	fun optionalString(key: String): String? {
-		return getOptional(key) as? String
-	}
-
-
-	fun optionalByteArray(key: String): ByteArray? {
-		return getOptional(key) as? ByteArray
-	}
-
-	fun optionalShortArray(key: String): ShortArray? {
-		return getOptional(key) as? ShortArray
-	}
-
-	fun optionalIntArray(key: String): IntArray? {
-		return getOptional(key) as? IntArray
-	}
-
-	fun optionalLongArray(key: String): LongArray? {
-		return getOptional(key) as? LongArray
-	}
-
-	fun optionalCompound(key: String): Compound? {
-		return getOptional(key) as? Compound
-	}
-
-	fun optionalCollection(key: String): Collection<Any>? {
-		return (backingMap[key] as? NBTValue.NBTList<*>)?.value
-	}
-
-	fun optionalConvert(key: String): Any? {
-		return NBTConverter.getValue(this, key)
-	}
-
-	inline fun <reified T: Any> optionalConvertTyped(key: String): T? {
-		return optionalConvert(key) as? T
-	}
-
-
-	override fun byte(key: String, value: Byte) {
-		backingMap[key] = NBTValue.NBTByte(value)
-	}
-
-	override fun short(key: String, value: Short) {
-		backingMap[key] = NBTValue.NBTShort(value)
-	}
-
-	override fun int(key: String, value: Int) {
-		backingMap[key] = NBTValue.NBTInt(value)
-	}
-
-	override fun long(key: String, value: Long) {
-		backingMap[key] = NBTValue.NBTLong(value)
-	}
-
-	override fun float(key: String, value: Float) {
-		backingMap[key] = NBTValue.NBTFloat(value)
-	}
-
-	override fun double(key: String, value: Double) {
-		backingMap[key] = NBTValue.NBTDouble(value)
-	}
-
-	override fun string(key: String, value: String) {
-		backingMap[key] = NBTValue.NBTString(value)
-	}
-
-	override fun byteArray(key: String, value: ByteArray) {
-		backingMap[key] = NBTValue.NBTByteArray(value)
-	}
-
-	override fun shortArray(key: String, value: ShortArray) {
-		backingMap[key] = NBTValue.NBTIntArray(value.map(Short::toInt).toIntArray())
-	}
-
-	override fun intArray(key: String, value: IntArray) {
-		backingMap[key] = NBTValue.NBTIntArray(value)
-	}
-
-	override fun longArray(key: String, value: LongArray) {
-		backingMap[key] = NBTValue.NBTLongArray(value)
-	}
-
-	fun compound(key: String, value: Compound) {
-		backingMap[key] = NBTValue.NBTCompound(value)
-	}
-
-	inline fun compound(key: String, body: Compound.() -> Unit) {
-		compound(key, Compound(body))
+	inline fun editCompound(key: String, body: Compound.() -> Unit) {
+		defaultCompound(key) {}.apply(body)
 	}
 
 	fun collection(key: String, collection: Collection<Any>) {
@@ -239,9 +164,7 @@ class Compound: DataMap<String> {
 		backingMap[key] = NBTValue.NBTList(collection)
 	}
 
-	fun convert(key: String, value: Any) {
-		NBTConverter.setValue(this, key, value)
-	}
+	fun convert(key: String, value: Any) { NBTConverter.setValue(this, key, value) }
 
 	fun write(file: File) {
 		file.outputStream().buffered().use {
