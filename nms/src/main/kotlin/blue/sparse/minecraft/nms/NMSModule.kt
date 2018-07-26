470b807f75df62ab5271b89f6a925b5bce3fbb89
ParadoxBlue <crappygamer10@gmail.com>
2018-07-26 12:23:29 -0700

    BlockEntityTag hot fix.

--- nms/src/main/kotlin/blue/sparse/minecraft/nms/NMSModule.kt
+++ nms/src/main/kotlin/blue/sparse/minecraft/nms/NMSModule.kt
@@ -3,10 +3,15 @@ package blue.sparse.minecraft.nms
 import blue.sparse.minecraft.core.data.nbt.converter.NBTConverter
 import blue.sparse.minecraft.module.*
 import blue.sparse.minecraft.nms.api.*
+import blue.sparse.minecraft.nms.extensions.nbt
 import blue.sparse.minecraft.nms.util.ItemStackConverter
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockPlaceEvent
 
 @ModuleDefinition
-object NMSModule : Module {
+object NMSModule : Module, Listener {
 
 	override val type = ModuleType.NMS
 
@@ -17,12 +22,12 @@ object NMSModule : Module {
 	val particleNMS = create("ParticleImpl") as ParticleNMS
 	val blockNMS = create("BlockImpl") as BlockNMS
 	val entityNMS = create("EntityImpl") as EntityNMS
-	val characterNMS = create("CharacterImpl") as CharacterNMS
 
 	override fun onEnable() {
 		println("NMSModule enabled with ${nmsHandlers.size} NMS handlers.")
 		nmsHandlers.forEach(NMSHandler::onEnable)
 		NBTConverter.register(ItemStackConverter)
+		registerListener(this)
 	}
 
 	override fun onDisable() {
@@ -37,6 +42,16 @@ object NMSModule : Module {
 		val instance = getClass(name).constructors.first().newInstance() as NMSHandler
 		nmsHandlers.add(instance)
 		return instance
+	}
+
+	@EventHandler
+	fun blockPlace(e: BlockPlaceEvent) {
+		val item = e.itemInHand
+		if (item == null || item.type == Material.AIR)
+			return
+		
+		val blockEntityTag = item.nbt.optionalCompound("BlockEntityTag") ?: return
+		e.block.nbt = blockEntityTag
 	}
 
 }
