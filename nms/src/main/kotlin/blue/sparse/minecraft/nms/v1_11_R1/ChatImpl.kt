7852c6f570f895ca098bdab4c57c0519702b2682
Tom <tomhazredstone@hotmail.com>
2018-09-04 16:39:15 -0500

    Fake chat implementations.

---
+++ nms/src/main/kotlin/blue/sparse/minecraft/nms/v1_11_R1/ChatImpl.kt
@@ -0,0 +1,11 @@
+package blue.sparse.minecraft.nms.v1_11_R1
+
+import blue.sparse.minecraft.nms.api.ChatNMS
+import blue.sparse.minecraft.nms.chat.Text
+import org.bukkit.entity.Player
+
+class ChatImpl: ChatNMS {
+	override fun send(player: Player, text: Text) {
+		TODO("not implemented")
+	}
+}
---
+++ nms/src/main/kotlin/blue/sparse/minecraft/nms/v1_8_R3/ChatImpl.kt
@@ -0,0 +1,11 @@
+package blue.sparse.minecraft.nms.v1_8_R3
+
+import blue.sparse.minecraft.nms.api.ChatNMS
+import blue.sparse.minecraft.nms.chat.Text
+import org.bukkit.entity.Player
+
+class ChatImpl: ChatNMS {
+	override fun send(player: Player, text: Text) {
+		TODO("not implemented")
+	}
+}
